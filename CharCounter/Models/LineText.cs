30aa292156617561ee589ef41c54c557c7c94d4e
Akn <sibata486@gmail.com>
2022-07-26 00:17:14 +0900

    add / ビューに読み込んだテキストを表示する処理を実装

--- CharCounter/Models/LineText.cs
+++ CharCounter/Models/LineText.cs
@@ -7,11 +7,14 @@ namespace CharCounter.Models
         private string text;
         private bool marked;
         private int index;
+        private bool ignore;
 
         public string Text { get => text; set => SetProperty(ref text, value); }
 
         public bool Marked { get => marked; set => SetProperty(ref marked, value); }
 
         public int Index { get => index; set => SetProperty(ref index, value); }
+
+        public bool Ignore { get => ignore; set => SetProperty(ref ignore, value); }
     }
 }
